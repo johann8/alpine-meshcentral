1461b412cab4ac1f2d1860e59f3e0dbb523bad7b
johann8 <jo.hahn@gmx.de>
2023-04-22 19:28:28 +0200

    added options TLSOffload and TrustedProxy

--- startup.sh
+++ startup.sh
@@ -7,39 +7,45 @@ else
    cp config.json.template meshcentral-data/${CONFIG_FILE}
 
    # use mongodb
-   if [ -n "$USE_MONGODB" ] && [ "$USE_MONGODB" == "true" ]; then
-       if [ -z "$MONGO_URL" ]; then
-           prefix=""
-           if [ -n "$MONGO_INITDB_ROOT_USERNAME" ] && [ -n "$MONGO_INITDB_ROOT_PASSWORD" ]; then
-               prefix="$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@"
+   if [ -n "${USE_MONGODB}" ] && [ "${USE_MONGODB}" == "true" ]; then
+       if [ -z "${MONGO_URL}" ]; then
+           PREFIX=""
+           if [ -n "${MONGO_INITDB_ROOT_USERNAME}" ] && [ -n "${MONGO_INITDB_ROOT_PASSWORD}" ]; then
+               PREFIX="${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@"
            fi
-           MONGO_URL="${prefix}mongodb:27017"
+           MONGO_URL="${PREFIX}mongodb:27017"
        fi
-       sed -i "s/\"_mongoDb\": null/\"mongoDb\": \"mongodb:\/\/$MONGO_URL\"/" meshcentral-data/${CONFIG_FILE}
+       sed -i "s/\"_mongoDb\": null/\"mongoDb\": \"mongodb:\/\/${MONGO_URL}\"/" meshcentral-data/${CONFIG_FILE}
    fi
-   sed -i "s/\"cert\": \"myserver.mydomain.com\"/\"cert\": \"$HOSTNAME\"/" meshcentral-data/${CONFIG_FILE}
-   sed -i "s/\"NewAccounts\": true/\"NewAccounts\": \"$ALLOW_NEW_ACCOUNTS\"/" meshcentral-data/${CONFIG_FILE}
-   sed -i "s/\"localSessionRecording\": false/\"localSessionRecording\": $LOCALSESSIONRECORDING/" meshcentral-data/${CONFIG_FILE}
-   sed -i "s/\"minify\": true/\"minify\": $MINIFY/" meshcentral-data/${CONFIG_FILE}
-   sed -i "s/\"WebRTC\": false/\"WebRTC\": \"$WEBRTC\"/" meshcentral-data/${CONFIG_FILE}
-   sed -i "s/\"AllowFraming\": false/\"AllowFraming\": \"$IFRAME\"/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"cert\": \"myserver.mydomain.com\"/\"cert\": \"${HOSTNAME}\"/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"NewAccounts\": true/\"NewAccounts\": \"${ALLOW_NEW_ACCOUNTS}\"/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"localSessionRecording\": false/\"localSessionRecording\": ${LOCALSESSIONRECORDING}/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"minify\": true/\"minify\": ${MINIFY}/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"WebRTC\": false/\"WebRTC\": \"${WEBRTC}\"/" meshcentral-data/${CONFIG_FILE}
+   sed -i "s/\"AllowFraming\": false/\"AllowFraming\": \"${IFRAME}\"/" meshcentral-data/${CONFIG_FILE}
+
+   # set rp traefik
+   if [ -n "${USE_TRAEFIK}" ] && [ "${USE_TRAEFIK}" == "true" ]; then
+      TRAEFIK_NAME=traefik
+      sed -i "s/\"TLSOffload\": false/\"TLSOffload\": \"${TRAEFIK_NAME}\"/" meshcentral-data/${CONFIG_FILE} 
+      sed -i "s/\"_TrustedProxy\": \"localhost\"/\"TrustedProxy\": \"${TRAEFIK_NAME}\"/" meshcentral-data/${CONFIG_FILE}
+   fi
 
    # create session key if empty 
    if [ -z "$SESSION_KEY" ]; then
-       #SESSION_KEY="$(cat /dev/urandom | tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' | fold -w 32 | head -n 1)"
        SESSION_KEY="$(cat /dev/urandom | tr -dc 'A-Za-z0-9!#%&()*+,-:;<=>?@_{|}~' | fold -w 40 | head -n 1)";
    fi
-   #sed -i "s/\"_sessionKey\": \"MyReallySecretPassword1\"/\"sessionKey\": \"$SESSION_KEY\"/" meshcentral-data/"${CONFIG_FILE}" 
+   # set sessionKey 
    sed -i '/"_sessionKey"/c\    "sessionKey": "'$SESSION_KEY'",' meshcentral-data/"${CONFIG_FILE}"   
 
    # set reverse proxy
-   if [ "$REVERSE_PROXY" != "false" ]; then
-      sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/$REVERSE_PROXY:$REVERSE_PROXY_TLS_PORT\"/" meshcentral-data/${CONFIG_FILE}
+   if [ "${REVERSE_PROXY}" != "false" ]; then
+      sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/${REVERSE_PROXY}:${REVERSE_PROXY_TLS_PORT}\"/" meshcentral-data/${CONFIG_FILE}
       node node_modules/meshcentral
       exit 0
    fi
    
-   node node_modules/meshcentral --cert "$HOSTNAME"
+   node node_modules/meshcentral --cert "${HOSTNAME}"
 fi
 
 # Set timezone variable
@@ -53,7 +59,7 @@ if ! [ -z ${TZ} ]; then
   # delete file localtime if exist and create new one
   if [ -f /etc/localtime ] ; then
     rm /etc/localtime
-    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+    ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
   fi
 fi
 
