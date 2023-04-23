216ae66efbf4ee5f055c47365b8e735d5d5d00bd
johann8 <jo.hahn@gmx.de>
2023-04-23 10:20:28 +0200

    added more config parameters

--- startup.sh
+++ startup.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 # set -x
 
+# functions
+welcome() {
+   echo "+----------------------------------------------------------+"
+   echo "|                                                          |"
+   echo "|              Welcome to Meshcentral Docker!              |"
+   echo "|                                                          |"
+   echo "+----------------------------------------------------------+"
+}
+
+# Set timezone variable
+if ! [ -z ${TZ} ]; then
+
+  # delete file timezone if exist
+  if [ -f /etc/timezone ] ; then
+    rm /etc/timezone
+  fi
+
+  # delete file localtime if exist and create new one
+  if [ -f /etc/localtime ] ; then
+    rm /etc/localtime
+    ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+  fi
+fi
+
+# configure config.json
 if [ -f "meshcentral-data/${CONFIG_FILE}" ]; then
+   welcome
    node node_modules/meshcentral
 else
    cp config.json.template meshcentral-data/${CONFIG_FILE}
@@ -15,8 +41,15 @@ else
            fi
            MONGO_URL="${PREFIX}mongodb:27017/${MONGO_DB_NAME}"
        fi
-       sed -i "s/\"_mongoDb\": null/\"mongoDb\": \"mongodb:\/\/${MONGO_URL}\",/" meshcentral-data/${CONFIG_FILE}
+       sed -i "s/\"_mongoDb\": null/\"mongoDb\": \"mongodb:\/\/${MONGO_URL}\"/" meshcentral-data/${CONFIG_FILE}       
+       sed -i "s/\"_MongoDbName\": \"meshcentral\"/\"MongoDbName\": \"${MONGO_DB_NAME}\"/" meshcentral-data/${CONFIG_FILE}
+       sed -i "s/\"_Mongodbcol\": \"meshcentral\"/\"Mongodbcol\": \"${MONGO_DB_NAME}\"/" meshcentral-data/${CONFIG_FILE}
+       sed -i "s/\"_MongoDbBulkOperations\": true/\"MongoDbBulkOperations\": true/" meshcentral-data/${CONFIG_FILE}
+       DB_ENCRYPT_KEY=$(pwgen -s 55 1)
+       sed -i "s/\"_DbEncryptKey\": \"MySuperString123\"/\"DbEncryptKey\": \"${DB_ENCRYPT_KEY}\"/" meshcentral-data/${CONFIG_FILE}
    fi
+
+   # set parameters 
    sed -i "s/\"cert\": \"myserver.mydomain.com\"/\"cert\": \"${HOSTNAME}\"/" meshcentral-data/${CONFIG_FILE}
    sed -i "s/\"NewAccounts\": true/\"NewAccounts\": \"${ALLOW_NEW_ACCOUNTS}\"/" meshcentral-data/${CONFIG_FILE}
    sed -i "s/\"localSessionRecording\": false/\"localSessionRecording\": ${LOCALSESSIONRECORDING}/" meshcentral-data/${CONFIG_FILE}
@@ -41,25 +74,11 @@ else
    # set reverse proxy
    if [ "${REVERSE_PROXY}" != "false" ]; then
       sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/${REVERSE_PROXY}:${REVERSE_PROXY_TLS_PORT}\"/" meshcentral-data/${CONFIG_FILE}
+      welcome
       node node_modules/meshcentral
       exit 0
    fi
-   
+   welcome
    node node_modules/meshcentral --cert "${HOSTNAME}"
-fi
-
-# Set timezone variable
-if ! [ -z ${TZ} ]; then
-
-  # delete file timezone if exist
-  if [ -f /etc/timezone ] ; then
-    rm /etc/timezone
-  fi
-
-  # delete file localtime if exist and create new one
-  if [ -f /etc/localtime ] ; then
-    rm /etc/localtime
-    ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
-  fi
 fi
 
